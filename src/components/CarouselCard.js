bc27a4ddffe5e05d96bafcebe31b0565e1b01f83
jaravad <jaravad@uninorte.edu.co>
2020-04-12 13:41:22 -0500

    carousel finished

--- src/components/CarouselCard.js
+++ src/components/CarouselCard.js
@@ -1,22 +1,20 @@
 import React from 'react';
-import customers from '../assets/images/customers.jpg';
-function CarouselCard() {
+
+function CarouselCard(props) {
   return (
-    <article className="carousel-card w-full p-10 flex flex-col-reverse md:flex-row items-center">
+    <article className="h-auto carousel-card w-full p-10 flex flex-col-reverse md:flex-row items-center">
       <div className="w-full md:w-1/2 text-gray-900 flex items-center justify-center">
-        <div className="w-4/5">
-          <h3 className="text-2xl font-semibold mb-4">
-            Complace a tus clientes
-          </h3>
-          <p>
-            Con las entregas confiables de los Socios Repartidores que usan la
-            plataforma RAPPIdos, podrás deleitar a los clientes con su comida
-            preferida, cuándo y dónde la quieran.
-          </p>
+        <div className="w-full md:w-4/5">
+          <h3 className="text-2xl font-semibold mb-4">{props.title}</h3>
+          <p className="font-light">{props.p}</p>
         </div>
       </div>
-      <div className="w-full md:w-1/2 ">
-        <img src={customers} alt="" className=" object-cover" />
+      <div className="w-full md:w-1/2 mb-6 md:mb-0">
+        <img
+          src={props.image}
+          alt=""
+          className="object-cover rounded shadow-lg"
+        />
       </div>
     </article>
   );
--- src/components/LandingCarousel.js
+++ src/components/LandingCarousel.js
@@ -1,14 +1,46 @@
 import React from 'react';
 import Carousel, { Dots } from '@brainhubeu/react-carousel';
 import '@brainhubeu/react-carousel/lib/style.css';
-import image from '../assets/images/hero-image.png';
+import customers from '../assets/images/customers.jpg';
+import food from '../assets/images/food.jpg';
+import admin from '../assets/images/admin.jpg';
 import CarouselCard from './CarouselCard';
-
+const info = [
+  {
+    title: 'Complace a tus clientes',
+    p:
+      'Con las entregas confiables de los Socios Repartidores que usan la plataforma RAPPIdos, podrás deleitar a los clientes con su comida preferida, cuándo y dónde la quieran.',
+    image: customers,
+  },
+  {
+    title: 'Abre la puerta al crecimiento',
+    p:
+      'Miles de usuarios de la app RAPPIdos pueden estar buscando comida en tu área. Al asociarte con RAPPIdos y agregar tu restaurante a la plataforma, podemos ayudarte a llegar a ellos.',
+    image: food,
+  },
+  {
+    title: 'Administra todo con facilidad',
+    p:
+      'Los pedidos circulan sin problemas con el software para restaurantes de Uber Eats, que brinda opciones de integración flexibles y ofrece soporte cuando lo necesitas.',
+    image: admin,
+  },
+];
 function LandingCarousel() {
+  const cards = info.map(function (text, index) {
+    return (
+      <CarouselCard
+        title={text.title}
+        p={text.p}
+        image={text.image}
+        key={index}
+      ></CarouselCard>
+    );
+  });
+
   return (
     <section className="w-4/5 mx-auto">
-      <Carousel className="w-full" centered infinite arrows>
-        <CarouselCard></CarouselCard>
+      <Carousel className="w-full my-8" centered infinite arrows>
+        {cards}
       </Carousel>
     </section>
   );
