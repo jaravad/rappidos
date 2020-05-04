ebc653ba2c67a038daa38e162598fb9115a90d99
jaravad <jaravad@uninorte.edu.co>
2020-05-04 17:18:35 -0500

    readme changed

--- src/components/landing/CarouselCard.js
+++ src/components/landing/CarouselCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function CarouselCard(props) {
   return (
-    <article className="h-auto carousel-card w-full p-10 flex flex-col-reverse md:flex-row items-center">
+    <article className="h-auto carousel__card w-full p-10 flex flex-col-reverse md:flex-row items-center">
       <div className="w-full md:w-1/2 flex items-center justify-center">
         <div className="w-full md:w-4/5">
           <h3 className="text-2xl font-semibold mb-4">{props.title}</h3>
