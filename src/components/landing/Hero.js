874cfca9c767469c8f90e6353a8c172fd66da272
jaravad <jaravad@uninorte.edu.co>
2020-05-19 10:20:01 -0500

    minor: hero animations added

--- src/components/landing/Hero.js
+++ src/components/landing/Hero.js
@@ -6,14 +6,14 @@ function Hero() {
     <header>
       <main className="hero-section w-full h-auto md:h-screen flex flex-col-reverse md:flex-row items-center bg-gray-100 pt-10">
         <div className="w-full md:w-1/2 mb-8 md:mb-0 flex justify-center items-center">
-          <img className="w-3/5 md:w-3/4" src={heroImg} alt="Food" />
+          <img className="w-3/5 md:w-3/4 relative" src={heroImg} alt="Food" />
         </div>
-        <div className="pt-16 mb-10 md:mb-0 pl-12 md:pl-5 md:pt-0 w-full md:w-1/2 flex flex-col justify-center text-gray-800">
+        <div className="pt-16 mb-10 md:mb-0 pl-12 md:pl-5 md:pt-0 w-full md:w-1/2 flex flex-col justify-center text-transparent">
           <div className="w-4/5">
-            <h1 className="text-5xl font-normal leading-tight mb-4">
+            <h1 className="text-5xl font-normal leading-tight mb-4 relative">
               Tu oportunidad de crecer está en <strong>RAPPIdos</strong>
             </h1>
-            <p className="leading-tight">
+            <p className="leading-tight relative">
               Únete hoy a nuestra comunidad de restaurantes. Ayudarte a crecer
               como empresa es uno de nuestro principales objetivos.
             </p>
