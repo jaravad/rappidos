61c9fb932dc18341bb2ae3fa549a20d2e2f528f3
jaravad <jaravad@uninorte.edu.co>
2020-05-27 04:19:10 -0500

    fix: no warnings

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import NavBar from './components/NavBar';
 import Landing from './components/landing/Landing';
-import LogIn from './components/login/LogIn';
 import SignupLogin from './components/signup-login/SignupLogin';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import PlatesPage from './components/plates/PlatesPage';
@@ -13,7 +12,6 @@ function App() {
       <Switch>
         <Route path="/" exact component={Landing} />
         <Route path="/#how-it-works" />
-        <Route path="/login" component={LogIn} />
         <Route path="/signup-login" component={SignupLogin} />
         <Route path="/platos" component={PlatesPage} />
       </Switch>
