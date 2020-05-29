0afce3f30102061504b06ca0bc54b15b3bb54198
jaravad <jaravad@uninorte.edu.co>
2020-05-29 03:43:58 -0500

    minor: plate card ui improved

--- src/components/plates/Plate.js
+++ src/components/plates/Plate.js
@@ -2,37 +2,55 @@ import React from 'react';
 
 function Plate(props) {
   return (
-    <article className="p-3 w-1/2 md:w-1/3 lg:w-1/4 ">
+    <article className="p-3 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 ">
       <div className="w-full rounded-md overflow-hidden shadow-md bg-white relative">
-        <img
-          className="w-full object-contain"
-          src={props.image}
-          alt={props.title}
-        />
+        <div className="w-full h-40">
+          <img
+            className="w-full object-cover object-center"
+            src={props.image}
+            alt={props.title}
+          />
+        </div>
+
         <div className="px-6 pt-5 pb-3">
-          <h3 className="font-bold mb-2 text-xl">{props.name}</h3>
-          <p className="text-gray-600 text-xs tracking-wide mb-1">
+          <h3 className="font-bold text-xl">{props.name}</h3>
+
+          <hr className="w-full border-top border-solid border-gray-300 mt-1 mb-2" />
+
+          <p className="text-gray-600 text-xs tracking-wide">
             {props.description}
           </p>
-          <span className="flex items-center mb-1 text-orange-600">
+
+          <hr className="w-full border-top border-solid border-gray-200 mt-2 mb-1" />
+
+          <span className="flex items-center text-orange-600">
             <span className="material-icons mr-1">whatshot</span>
-            <span className="pt-1">{`${props.calories} ${
-              parseInt(props.calories) > 1 ? 'calorias' : 'caloría'
-            }`}</span>
+            <span className="pt-1 flex items-center">
+              <strong className="text-xl mr-1">{props.calories} </strong>
+              <span className="text-xs">{`${
+                parseInt(props.calories) > 1 ? 'calorias' : 'caloría'
+              }`}</span>
+            </span>
           </span>
 
-          <span className="flex items-center mb-1">
+          <span className="flex items-center">
             <span className="material-icons mr-1">people</span>
-            <span className="pt-1">{`${props.people} ${
-              parseInt(props.people) > 1 ? 'personas' : 'persona'
-            }`}</span>
+            <span className="pt-1 flex items-center">
+              <strong className="text-xl mr-1">{props.people} </strong>
+              <span className="text-xs">{`${
+                parseInt(props.people) > 1 ? 'personas' : 'persona'
+              }`}</span>
+            </span>
           </span>
 
-          <span className="flex items-center mb-1">
+          <span className="flex items-center">
             <span className="material-icons mr-1">fastfood</span>
-            <span className="pt-1">{`${props.weight} ${
-              parseInt(props.weight) > 1 ? 'gramos' : 'gramo'
-            }`}</span>
+            <span className="pt-1 flex items-center">
+              <strong className="text-xl mr-1">{props.weight} </strong>
+              <span className="text-xs">{`${
+                parseInt(props.weight) > 1 ? 'gramos' : 'gramo'
+              }`}</span>
+            </span>
           </span>
 
           <span className="flex items-baseline">
